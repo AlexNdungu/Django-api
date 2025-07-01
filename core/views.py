7a7574a324fc9941177d32fb42be3ed50157a933
AlexNdungu <alexmeta517@gmail.com>
2025-07-01 07:19:21 +0300

    add api key

--- core/views.py
+++ core/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework import generics
+from rest_framework_api_key.permissions import HasAPIKey
+
 
 # Create your views here.
 
 class ProductListView(generics.ListAPIView):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+    permission_classes = [HasAPIKey]
